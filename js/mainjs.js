8e0bf9a551b8f8a97bd31a424819096afeeaca7a
Sahil Aggrawal <innocentcoder5497@gmail.com>
2020-06-12 15:17:33 +0530

    Design Complete

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,5 +1,19 @@
 var app = document.getElementById('headtext');
 
+$("#sendmail").click(function (){
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var body = $("#ebody").val();
+    var myemail = 'sahil13p@gmail.com';
+    var complete_body = 'From:%0D%0AName : ' + name + '%0D%0AEmail : ' + email + '%0D%0A%0D%0AMeassage%0D%0A%0D%0A' + body + '%0D%0A%0D%0A';
+
+    var mailto_link = 'mailto:' + myemail + '?subject=' + subject + '&body=' + complete_body;
+    win = window.open(mailto_link, 'emailWindow');
+    if (win && win.open && !win.closed) win.close();
+})
+
+
 var typewriter = new Typewriter(app, {
     loop: true
 });
